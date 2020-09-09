82cb35bc3b1939f46c93ec9316e5732f1e3555d1
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-09-09 15:21:45 +0300

    Updates

---
+++ precache-manifest.9deb7bd2af6b11107faf6fb65da9ff09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83edbcd00ae6a44fc926ae97977de7a",
+    "url": "/rasiely/index.html"
+  },
+  {
+    "revision": "f5651ed4c52e0669eeca",
+    "url": "/rasiely/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "610238b09063b5494a86",
+    "url": "/rasiely/static/css/main.c965b468.chunk.css"
+  },
+  {
+    "revision": "f5651ed4c52e0669eeca",
+    "url": "/rasiely/static/js/2.19c71235.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rasiely/static/js/2.19c71235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610238b09063b5494a86",
+    "url": "/rasiely/static/js/main.3046994e.chunk.js"
+  },
+  {
+    "revision": "90ecc6e8d58f46b7c170",
+    "url": "/rasiely/static/js/runtime-main.c46267b0.js"
+  }
+]);
